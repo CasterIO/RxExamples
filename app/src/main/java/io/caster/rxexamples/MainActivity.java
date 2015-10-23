95c485a7ef6b0ed27a1ceebc5c1066b7c23f7de9
Donn Felker <donn@donnfelker.com>
2015-10-23 16:06:25 -0400

    017 Episode code.

--- app/src/main/java/io/caster/rxexamples/MainActivity.java
+++ app/src/main/java/io/caster/rxexamples/MainActivity.java
@@ -1,8 +1,11 @@
 package io.caster.rxexamples;
 
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -13,65 +16,97 @@ import com.squareup.okhttp.Response;
 import java.io.IOException;
 import java.util.Map;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func0;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Subscription subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getGistObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Gist>() {
+                    @Override
+                    public void onCompleted() {
 
-        new AsyncTask<Void, Void, Gist>() {
-
-            public IOException error;
-
-            @Override
-            protected Gist doInBackground(Void... params) {
-                OkHttpClient client = new OkHttpClient();
-
-                // Go get this Gist: https://gist.github.com/donnfelker/db72a05cc03ef523ee74
-                // via the GitHub API
-                Request request = new Request.Builder()
-                        .url("https://api.github.com/gists/db72a05cc03ef523ee74")
-                        .build();
-
-                try {
-                    Response response = client.newCall(request).execute();
-
-                    if (response.isSuccessful()) {
-                        Gist gist = new Gson().fromJson(response.body().charStream(), Gist.class);
-                        return gist;
                     }
 
-                    return null;
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, e.getMessage(), e);
+                    }
 
-                } catch (IOException e) {
-                    this.error = e;
-                    return null;
-                }
-            }
+                    @Override
+                    public void onNext(Gist gist) {
+                        StringBuilder sb = new StringBuilder();
+                        // Output
+                        for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
+                            sb.append(entry.getKey());
+                            sb.append(" - ");
+                            sb.append("Length of file ");
+                            sb.append(entry.getValue().content.length());
+                            sb.append("\n");
+                        }
 
+                        TextView text = (TextView) findViewById(R.id.main_message);
+                        text.setText(sb.toString());
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+        }
+    }
+
+    @Nullable
+    private Gist getGist() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        // Go get this Gist: https://gist.github.com/donnfelker/db72a05cc03ef523ee74
+        // via the GitHub API
+        Request request = new Request.Builder()
+                .url("https://api.github.com/gists/db72a05cc03ef523ee74")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            Gist gist = new Gson().fromJson(response.body().charStream(), Gist.class);
+            return gist;
+        }
+
+        return null;
+
+
+    }
+
+    public Observable<Gist> getGistObservable() {
+        return Observable.defer(new Func0<Observable<Gist>>() {
             @Override
-            protected void onPostExecute(Gist gist) {
-                super.onPostExecute(gist);
-
-                StringBuilder sb = new StringBuilder();
-                // Output
-                for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
-                    sb.append(entry.getKey());
-                    sb.append(" - ");
-                    sb.append("Length of file ");
-                    sb.append(entry.getValue().content.length());
-                    sb.append("\n");
+            public Observable<Gist> call() {
+                try {
+                    return Observable.just(getGist());
+                } catch (IOException e) {
+                    return null;
                 }
-
-                TextView text = (TextView) findViewById(R.id.main_message);
-                text.setText(sb.toString());
             }
-        }.execute();
-
+        });
     }
 
 }
