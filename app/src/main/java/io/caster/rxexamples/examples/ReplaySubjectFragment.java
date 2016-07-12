b10d0d53e1351299fd21c7e9f3a6f7b53f5f6031
Donn Felker <donn@donnfelker.com>
2016-07-11 22:22:17 -0400

    Adding timed replay subject

--- app/src/main/java/io/caster/rxexamples/examples/ReplaySubjectFragment.java
+++ app/src/main/java/io/caster/rxexamples/examples/ReplaySubjectFragment.java
@@ -7,12 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.concurrent.TimeUnit;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.caster.rxexamples.R;
 import io.caster.rxexamples.models.Stock;
 import rx.Subscriber;
 import rx.Subscription;
+import rx.schedulers.Schedulers;
 import rx.subjects.ReplaySubject;
 import rx.subscriptions.CompositeSubscription;
 import timber.log.Timber;
@@ -96,6 +99,49 @@ public class ReplaySubjectFragment extends Fragment {
         Subscription tardySubscription = stockReplaySubject.subscribe(getTardySubscriber());
         compositeSubscription.add(tardySubscription);
 
+    }
+
+
+    @OnClick(R.id.replay_subject_timed)
+    public void onReplaySubjectTimed() {
+
+        try {
+
+            unsubscribe();
+            createSubscription();
+
+            // A replay subject that will only replay the last two items.
+            ReplaySubject<Stock> stockReplaySubject = ReplaySubject.createWithTime(250, TimeUnit.MILLISECONDS, Schedulers.immediate());
+
+            stockReplaySubject.onNext(new Stock(GOOG, 715.09));
+            Thread.sleep(100);
+            stockReplaySubject.onNext(new Stock(GOOG, 716.00));
+            Thread.sleep(100);
+            stockReplaySubject.onNext(new Stock(GOOG, 714.00));
+            Thread.sleep(100);
+
+            // Only the last two items will be replayed to this subscriber (716 and 714)
+            // because the first one has already expired.
+            Subscription defaultSub = stockReplaySubject.subscribe(getDefaultSubscriber());
+            compositeSubscription.add(defaultSub); // All three values will be delivered.
+
+            // This will also be emitted to the defaultSub above.
+            stockReplaySubject.onNext(new Stock(GOOG, 720));
+            Thread.sleep(100);
+            stockReplaySubject.onCompleted(); // Terminate the stream with a completed event.
+
+            // Lets sleep for another 100 millis to simulate some time passing.
+            Thread.sleep(100);
+
+            // Subscribe again with a new subscriber. This time the only item that is valid is
+            // the last item: '720' as all the others have expired.
+            Subscription tardySubscription = stockReplaySubject.subscribe(getTardySubscriber());
+            compositeSubscription.add(tardySubscription);
+
+
+        } catch (InterruptedException ex) {
+            Timber.e(ex, ex.getMessage());
+        }
     }
 
     private void createSubscription() {
