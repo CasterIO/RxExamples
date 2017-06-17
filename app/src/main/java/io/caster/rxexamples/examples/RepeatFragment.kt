0cb504d0f99bb692939b0d0eb6ccc80f61cd434a
Donn Felker <donn@donnfelker.com>
2017-06-16 21:46:53 -0400

    Using RxJava2 on Repeat example.

--- app/src/main/java/io/caster/rxexamples/examples/RepeatFragment.kt
+++ app/src/main/java/io/caster/rxexamples/examples/RepeatFragment.kt
@@ -1,49 +1,55 @@
 package io.caster.rxexamples.examples
 
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import io.caster.rxexamples.R
-import rx.Observable
-import rx.Subscriber
-import rx.Subscription
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import timber.log.Timber
 
 class RepeatFragment : Fragment() {
 
-    lateinit var sub: Subscription
+    lateinit var disposable: Disposable
+
+    lateinit var content: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_repeat, container, false)
+        content = view.findViewById(R.id.repeat_content) as TextView
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        sub = Observable.just("Foo", "Bar", "Fiz", "Bin")
-                .repeat(5) // This will force the sequence to repeat 5 times in a row.
-                .subscribe(object : Subscriber<String>() {
-                    override fun onError(e: Throwable?) {
-                        Timber.e(e, e?.message)
-                    }
-
-                    override fun onCompleted() {
-
-                    }
-
-                    override fun onNext(item: String?) {
-                        Timber.d("Value $item, Time: ${System.currentTimeMillis()}")
-                    }
-
-                })
+        disposable =
+                Observable.just("one", "two", "three", "four")
+                        .repeat(5) // This will force the sequence to repeat 5 times in a row.
+                        .subscribe({ item ->
+                            // Consumer<String>
+                            // onNext
+                            content.text = "${content.text}\n$item"
+                            Timber.d("Value $item, Time: ${System.currentTimeMillis()}")
+                        }, { ex ->
+                            // Consumer<? extends Throwable>
+                            // onError
+                            Timber.e(ex, "Oh No!")
+                        }, {
+                            // Action
+                            // onComplete
+                            Timber.i("onComplete")
+                            content.text = "${content.text}\nonComplete"
+                        })
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        sub?.unsubscribe()
+        disposable.dispose()
     }
 }
